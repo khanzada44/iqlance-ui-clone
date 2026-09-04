a456fe3d466641806e9164290c7dac36180af13f
minham Hussain <minhamkhanzada4455@gmail.com>
2026-09-04 07:01:52 -0700

    chnages

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body>
         {/* <DisableRightClick> */}
           {children}
+          
         {/* </DisableRightClick> */}
       </body>
     </html>
