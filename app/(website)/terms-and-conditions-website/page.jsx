461df83d1ef1cc71505978c4c192dd199f66ea53
minham Hussain <minhamkhanzada4455@gmail.com>
2026-09-14 08:49:56 -0700

    changes

--- app/(website)/terms-and-conditions-website/page.jsx
+++ app/(website)/terms-and-conditions-website/page.jsx
@@ -1,4 +1,4 @@
-import TermsAndConditions from "../../components/pages/terms-and-conditions/page";
+import TermsAndConditions from "../../components/pages/terms-and-conditions-website/page";
 
 export default function Devapp() {
   return <TermsAndConditions />;
